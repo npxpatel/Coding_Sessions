a8db9632e6b478a2588d758ff63803505c78974f
Nirajkumar Patel <128296263+npxpatel@users.noreply.github.com>
2025-03-15 20:06:26 +0530

    Time: 300 ms (15.77%), Space: 45.9 MB (97.75%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    
-   static bool customSort(string &a, string &b){
-        return a.size() < b.size();
-    }
+    static bool customSort(string& a, string& b) { return a.size() < b.size(); }
 
-    bool isConcatenated(unordered_map<string, bool>&present, string &word, int idx, vector<int>&dp){
-        if(idx == word.size()) return true;
+    bool isConcatenated(unordered_map<string, bool>& present, string& word,
+                        int idx, vector<int>& dp) {
+        if (idx == word.size())
+            return true;
 
-        if(dp[idx] != -1) return dp[idx];
+        if (dp[idx] != -1)
+            return dp[idx];
 
         string curr = "";
         int n = word.size();
         bool okay = false;
-        for(int i = idx; i < n; i ++){
+        for (int i = idx; i < n; i++) {
             curr += word[i];
-            if(present[curr]){
+            if (present[curr]) {
                 okay = okay || isConcatenated(present, word, i + 1, dp);
             }
         }
@@ -23,39 +23,36 @@ public:
     }
 
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        if(words.size() == 1) return {};
-        unordered_map<string, bool>present;
+        if (words.size() == 1)
+            return {};
+        unordered_set<string> present(words.begin(), words.end());
         int n = words.size();
-        sort(words.begin(), words.end(), customSort);
-        vector<string>ans;
+        vector<string> ans;
 
-        vector<bool>dp(30 + 1, false);
-        
+        vector<bool> dp(30 + 1, false);
 
-        for(int i = 0; i < n; i ++){
-            
+        for (int i = 0; i < n; i++) {
+
             int size = words[i].size();
             string s = words[i];
             dp[size] = true;
 
-            for(int j = size - 1; j >= 0; j --){
+            for (int j = size - 1; j >= 0; j--) {
                 bool okay = false;
-                for(int k = j; k < size; k ++){
-                    if(present[s.substr(j, k - j + 1)]){
+                for (int k = j; k < size; k++) {
+                    if ( k - j + 1 != size &&  present.count(s.substr(j, k - j + 1)) ) {
                         okay = okay || dp[k + 1];
                     }
                 }
                 dp[j] = okay;
             }
 
-            if(dp[0]){
+            if (dp[0]) {
                 ans.push_back(s);
             }
 
-
-
             fill(dp.begin(), dp.end(), false);
-            present[words[i]] = true;
+  
         }
         return ans;
     }
